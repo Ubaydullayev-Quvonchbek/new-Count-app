b165edaf40ff0f29f3ad0d5be097ebd40242a771
Quvonchbek048 <quvonchbeku048@gmail.com>
2023-04-26 23:16:04 +0500

    Team Quvonchbek

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import "./index.css";
 import { Container, Row, Col } from "react-bootstrap";
+import audio1 from "./audio/audio1.wav";
+import audio2 from "./audio/audio2.wav";
 
 function App() {
   const [Count, setCount] = useState(0);
@@ -9,18 +11,32 @@ function App() {
       setCount(0);
     } else {
       setCount(Count + 1);
+      new Audio(audio1).play()
     }
+  }
+  const res = () => {
+    setCount(0)
+    new Audio(audio2).play()
   }
   return (
     <>
-      <Container>
-        <Row className="justify-content-center">
-          <Col lg={4}>
-            <h1 className="Count">{Count}</h1>
-            <button className="inc" onClick={() => inc()}>+</button>
-          </Col>
-        </Row>
-      </Container>
+      <main>
+        <Container>
+          <Row className="justify-content-center align-items-center">
+            <Col lg={5}>
+              <div className="card">
+                <h1 className="Count">{Count}</h1>
+                <div className="d-flex align-items-center margin-auto">
+                  <button className="inc" onClick={() => inc()}>+</button>
+                  <button className="res" onClick={() => res()}>↻</button>
+                </div>
+              </div>
+              <audio className="audio1" src={audio1}></audio>
+              <audio className="audio2" src={audio2}></audio>
+            </Col>
+          </Row>
+        </Container>
+      </main>
     </>
   );
 }
